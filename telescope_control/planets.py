6cb0da1cdc715019aee8d052acb5337b0ea7a8f1
Shulin00 <shulin00@umail.ucsb.edu>
2017-07-18 12:13:55 -0700

    Update planets.py

--- telescope_control/planets.py
+++ telescope_control/planets.py
@@ -72,7 +72,7 @@ def radec_to_azalt(LOCATION,RA,DEC):
   
   
 
-def getlocation(LOCATION, CBODY, Az, El):
+def getlocation(LOCATION, CBODY):
 
   #observation locations
   locations = dict(
@@ -107,49 +107,42 @@ def getlocation(LOCATION, CBODY, Az, El):
   time = str(datetime.utcnow())
 
   #celestial body of interest
-  if CBODY=='Sky-Cor':
-    return az,alt
-  else:
-    
-    cbody = cbodies[CBODY]
+  if type(CBODY)==list:
+      az=CBODY[1]
+      alt=CBODY[2]
+      return az,alt
+  if type(CBODY)==str:
+      cbody = cbodies[CBODY]
+      #this method take 2x as long and produces a slightly different azel, I am not sure which is more accurate
+      '''##########################
+        #compute radec of body at current time
+        cbody.compute(time)
 
-
-    #this method take 2x as long and produces a slightly different azel, I am not sure which is more accurate
-    '''
-    ##########################
-    #compute radec of body at current time
-    cbody.compute(time)
-
-    #create icrs object to convert to az el
-    icrs = ICRS(ra = Angle(str(cbody.ra) + 'hours'), dec = Angle(str(cbody.dec) + 'degrees'))
-    #print icrs.ra.deg, icrs.dec.deg
-    #convert to altaz coordinates
-    altaz = icrs.transform_to(AltAz(obstime=t, location=location))
-    print altaz.az.deg, altaz.alt.deg
-    #############################
-    '''
-
-    #set observer location and epoch
-    obs = ephem.Observer()
-    obs.lon, obs.lat = str(location.longitude.deg), str(location.latitude.deg)
-    obs.elevation = float(str(location.height).split()[0])
-    obs.date = time
-
-    #compute celestial body az/el coordinates given observer spacetime coordiantes
-    cbody.compute(obs)
-    print cbody.az
-
-    #convert azimuth to degrees
-    az = str(cbody.az).split(':')
-    az = [float(i) for i in az]
-    az = (az[0] + az[1]/60. + az[2]/60./60.)
-
-    #convert altitude to degrees
-    alt = str(cbody.alt).split(':')
-    alt = [float(i) for i in alt]
-    alt = (alt[0] + alt[1]/60. + alt[2]/60./60.)
-
-    #return azimuth and altitude of celestial body
-    return az, alt
+        #create icrs object to convert to az el
+        icrs = ICRS(ra = Angle(str(cbody.ra) + 'hours'), dec = Angle(str(cbody.dec) + 'degrees'))
+        #print icrs.ra.deg, icrs.dec.deg
+        #convert to altaz coordinates
+        altaz = icrs.transform_to(AltAz(obstime=t, location=location))
+        print altaz.az.deg, altaz.alt.deg
+        #############################
+      '''
+      #set observer location and epoch
+      obs = ephem.Observer()
+      obs.lon, obs.lat = str(location.longitude.deg), str(location.latitude.deg)
+      obs.elevation = float(str(location.height).split()[0])
+      obs.date = time
+      #compute celestial body az/el coordinates given observer spacetime coordiantes
+      cbody.compute(obs)
+      print cbody.az
+      #convert azimuth to degrees
+      az = str(cbody.az).split(':')
+      az = [float(i) for i in az]
+      az = (az[0] + az[1]/60. + az[2]/60./60.)
+      #convert altitude to degrees
+      alt = str(cbody.alt).split(':')
+      alt = [float(i) for i in alt]
+      alt = (alt[0] + alt[1]/60. + alt[2]/60./60.)
+      #return azimuth and altitude of celestial body
+      return az, alt
 if __name__=='__main__':
   getlocation(LOCATION='UCSB',CBODY='Sun',Az=None,El=None)
